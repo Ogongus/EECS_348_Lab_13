f2854e00cd49af33c54c9325ed5f9267de279a35
Ogongus <whjtan4357@gmail.com>
2025-11-25 17:20:11 -0600

    add correct

--- lab13_debugging.cpp
+++ lab13_debugging.cpp
@@ -10,4 +10,11 @@ bool faillecture_faulty(const vector<int>& attendance_records) {
     return absent_count >= 3;
 }
 
-bool faillecture_correct(const vector)
+bool faillecture_correct(const vector<int>& attendance_records) {
+    int absent_count = 0;
+    for (int i = 0; i < attendance_records.size(); ++i) {
+        absent_count += attendance_records[i] == 0;
+    }
+    return absent_count >= 3;
+}
+
